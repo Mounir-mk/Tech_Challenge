6fb358aa839ff950c10b19c3611bbb5e292cd1c4
Mounir-mk <mounir.mekoui@gmail.com>
2023-01-23 20:06:45 +0100

    fixing member models for inserting data to db

--- backend/src/models/MemberManager.js
+++ backend/src/models/MemberManager.js
@@ -7,16 +7,16 @@ class MemberManager extends AbstractManager {
 
   insert(member) {
     return this.connection.query(
-      `insert into ${this.table} (title) values (?)`,
-      [member.title]
+      `insert into ${this.table} (name) values (?)`,
+      [member.name]
     );
   }
 
   update(member) {
-    return this.connection.query(
-      `update ${this.table} set title = ? where id = ?`,
-      [member.title, member.id]
-    );
+    return this.connection.query(`update ${this.table} set ? where id = ?`, [
+      member,
+      member.id,
+    ]);
   }
 }
 
